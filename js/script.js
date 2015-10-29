d21c4e0dca01534239af13f12af3a25915b71dc8
Chandan Veerabhadrappa <chandanv89@gmail.com>
2015-10-29 17:58:48 +0530

    Worked on HTML layout and a few animations

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+   var bars = $("#wf-main .wf-bar");
+   var shadows = $("#wf-shadow .wf-bar");
+   
+   var animateBars = function(){
+      for(var i=0; i<bars.length; i++){
+         var height = Math.floor(Math.random()*1173)%50 + "px";
+         bars[i].style.height = shadows[i].style.height = height;
+      }
+      setTimeout(animateBars, 100000);
+   };
+   
+   animateBars();
+});
